8ebebe75f6d52b84699631809d36006866ac7d2b
MohamedBidouk <mohamedbidouk1998@gmail.com>
2022-03-07 18:02:28 +0100

    l'exercice deux éxucutable

--- src/exercise2/testPile.java
+++ src/exercise2/testPile.java
@@ -3,13 +3,23 @@ package exercise2;
 public class testPile {
 	public static void main(String[]args){
 		PileListe p = new PileListe();
-		for(int i = 0 ; i < 10 ; i++)
-		p.empiler(new Integer(i));
-		while(!p.estVide()){
-		System.out.println((Integer) p.dernier());
+		for(int i = 0 ; i < 10 ; i++) {
+			p.empiler(new Integer(i));
+			p.empiler("L'entier " + i);
+		}
+		for(int i = 0 ; i < 20 ; i++){
+		System.out.println(p.dernier());
 		p.depiler();
 		}
+		// Tester aussi p.empiler("L'entier " + i);
+		/* System.out.println((Integer) p.dernier());
 
+		=> Erreur à l'exécution de casting
+		=> System.out.println(p.dernier());
+		=> Ouverture sur les exceptions */
+		/* Remplacer while(!p.estVide()) par for(int i = 0 ; i < 20 ; i++)
+		=> Erreur à l'exécution lors du dépilement d'une pile vide
+		=> Ouverture sur les exceptions mais surtout sur la généricité */
 	}
 
 }
